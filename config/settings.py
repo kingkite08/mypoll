2c64615bb5b54f966b5c0557c41f4f58a8ed18a5
Jeon Hayeon <gkdusdl118@naver.com>
2025-01-15 15:22:12 +0900

    mysql설정

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,11 +81,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    "default":{
+        "ENGINE":"django.db.backends.mysql",
+        "NAME":"mypoll", # 연결할 DataBase이름
+        "USER":"admin", # 사용자 username
+        "PASSWORD":"ibgdrgn0818",
+        "HOST":"mypoll-db.cvs8aecwccdw.ap-northeast-2.rds.amazonaws.com",
+        "PORT":"3306"
     }
+    
 }
+
 
 
 # Password validation
